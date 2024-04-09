6bed3e5b5c5ac57b934d3198bd39035c1af0cb97
Israel Oluwayemi Joseph <97449656+Israelsgit@users.noreply.github.com>
2024-04-09 14:03:29 +0100

    changed logic

--- app.js
+++ app.js
@@ -1,17 +1,34 @@
+// Check if a number is prime or not
+function getPrime() {
+  const number = document.getElementById('input').value;
 
-function getRange() {
-    let x = document.getElementById('input').value;
+  if (isNaN(number)) {
+    document.getElementById('answer').innerText = 'Please enter a valid number.';
+    return;
+  }
+
+  if (number < 0) {
+    document.getElementById('answer').innerText = 'Please enter a positive number.';
+    return;
+  }
+
     let text;
-
-    if ( x % 2 === 0 ) {
-        text = "The number is even";
-    } 
-    else if (isNaN(x)) {
-        text = "invalid input"
+    
+  if (number === 1) {
+    text = `${number} is not a prime number.`;
+  } 
+    
+  else if (number > 1) {
+    for (let i = 2; i < number; i++) {
+      if (number % i === 0) {
+        text = `${number} is not a prime number`;
+        break;
+      }
     }
-    else {
-        text = "The number is odd";
-    }
 
-    document.getElementById('answer').innerHTML = text;
+    if (!text) {
+      text = `${number} is a prime number.`;
+    }
+  }
+  document.getElementById('answer').innerText = text;
 }
